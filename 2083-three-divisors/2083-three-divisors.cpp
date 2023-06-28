47caf5523c663414ea38367b21875edc970f2bde
RahulM021 <92747612+RahulM021@users.noreply.github.com>
2023-06-28 14:25:55 +0530

    Time: 0 ms (100%), Space: 5.9 MB (79.3%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.cpp
+++ 2083-three-divisors/2083-three-divisors.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int cnt=1;
-        for(int i=2;i<=n;i++)
-        if(n%i==0)
-        cnt++;
-
-        return cnt==3;
+           int i=1,c=0;
+        while(i<=sqrt(n)){
+            if( n%i==0){
+                if(n/i!=i)c+=2;
+                else c++;
+            }
+            i++;
+        }
+        cout<<c;
+        return c==3;
     }
 };
