1c8f483edd8e5ae35dfa550c80e613d080cdf5bb
RahulM021 <92747612+RahulM021@users.noreply.github.com>
2023-06-29 19:13:48 +0530

    Time: 228 ms (79.91%), Space: 57.8 MB (95.79%) - LeetHub

--- 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/1675-magnetic-force-between-two-balls.cpp
@@ -1,31 +1,36 @@
-class Solution {
-public:
-    bool place(vector<int>& a,int m,int d){
-        int cur=a[0];
-        m--;
-        for(int i=1;i<a.size();i++){
-            if((a[i]-cur)>=d){
-                m--;
-                cur=a[i];
-                if(m==0)
-                return true;
+class Solution
+{
+    public:
+        bool place(vector<int> &a, int m, int d)
+        {
+            int cur = a[0];
+            m--;
+            for (int i = 1; i < a.size(); i++)
+            {
+                if ((a[i] - cur) >= d)
+                {
+                    m--;
+                    cur = a[i];
+                    if (m == 0)
+                        return true;
+                }
             }
+            return m == 0;
         }
-        return m==0;
-    }
-    int maxDistance(vector<int>& position, int m) {
-        sort(position.begin(),position.end());
-        int lo=1;
-        int hi=*max_element(position.begin(),position.end());
+    int maxDistance(vector<int> &position, int m)
+    {
+        sort(position.begin(), position.end());
+        int lo = 1;
+        int hi = *max_element(position.begin(), position.end());
         int ans;
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            if(place(position,m,mid)){
-                ans=mid;
-            lo=mid+1;
-            }
+        while (lo <= hi)
+        {
+            int mid = (lo + hi) / 2;
+            if (place(position, m, mid))
+                lo = mid + 1;
+
             else
-            hi=mid-1;
+                hi = mid - 1;
         }
         return hi;
     }
